b53ebc880f12256a75e6713f94712b067c6e82b9
mereszeta <grzegorzszeremeta@gmail.com>
2017-06-28 02:20:25 +0200

    another thing added, another not tested XD

--- src/main/scala/ChannelsManagement.scala
+++ src/main/scala/ChannelsManagement.scala
@@ -8,8 +8,8 @@ import scala.collection.immutable.HashMap
 trait ChannelsManagement { this: Actor =>
   val channels:HashMap[String,List[String]]
   protected def chatManagement: Receive = {
-    case msg @ AddChannel(from,channel, _) => getSession(from,channel).foreach(_ ! msg)
-    case msg @ RemoveChannel(from,channel) =>     getSession(from,channel).foreach(_ forward msg)
+    case msg @ AddChannel(from,channel, _) => channels.updated(channel,List(from))
+    case msg @ RemoveChannel(from,channel) => channels.filterNot(_._1.equals(channel))
   }
 
 }
